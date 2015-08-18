fd0fb5dc95f60663523b55ff9749db09a6f61e24
paopow <ksiangliulue@gmail.com>
2015-08-18 12:34:11 -0400

    Add requirements.txt

--- app/app.py
+++ app/app.py
@@ -1,26 +1,19 @@
 from flask import Flask, jsonify
-import spacy.en
-from numpy import dot
-from numpy.linalg import norm
 
+from spacySim import spacySim
 app = Flask(__name__)
-nlp = spacy.en.English()
 
-def cossim(a, b):
-    return dot(a, b) / (norm(a) * norm(b))
 
 @app.route('/')
 def index():
     return "Hello, World!"
 
-
 @app.route('/spaCy/api/similarity/<word1>/<word2>', methods=['GET'])
 def get_spacy_sim(word1, word2):
-    tok1 = nlp(word1)[0]
-    tok2 = nlp(word2)[0]
-    sim = cossim(tok1.repvec, tok2.repvec)
-    print type(sim)
-    return jsonify({'word1': word1, 'word2': word2, 'similarity': float(sim)})
+    return jsonify({
+            'word1': word1,
+            'word2': word2,
+            'similarity': spacySim(word1, word2)})
 
 
 if __name__ == '__main__':
